31ff4d3ee2a47c738a37f1e047826661550d6106
DaRebel28 <johnburghardt23@gmail.com>
2026-06-14 16:10:40 -0400

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -18,18 +18,6 @@ plane[xAxis][0] = '←'
 plane[ROWS - 1][yAxis] = '↑'
 plane[0][yAxis] = '↓'
 plane[xAxis][yAxis] = '+'
-for x in range(-yAxis, ceil(COLS / 2)) :
-    try :
-        y = (1/4)*x
-        if True :
-            y = int(y)
-        else :
-            continue
-    except :
-        continue
-    if abs(y) <= xAxis :
-        plane[xAxis + y][yAxis + x] = '·'
-# Graphs f(x)
 while True :
     print("\033[H\033[2J", end = "")
     print("Graphing Calculator")
@@ -37,19 +25,6 @@ while True :
         for j in range(COLS) :
             print(plane[(ROWS - 1) - i][j], end = "")
         print()
-    """
-      01234
-      _____
-    0|  ↑  
-    1|  |  
-    2|←-+-→
-    3|  |  
-    4|  ↓  
-    xAxis = ceil(ROWS / 2) - 1 = ceil(5 / 2) - 1 = 3 - 1 = 2
-    yAxis = ceil(ROWS / 2) - 1 = ceil(5 / 2) - 1 = 3 - 1 = 2
-    xIndex = yAxis + user_x
-    yIndex = xAxis + user_y
-    """
     pair = input("Enter coordinates: ")
     try :
         x = int(pair[pair.find('(') + 1:pair.find(',')])
